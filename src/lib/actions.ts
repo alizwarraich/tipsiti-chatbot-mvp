89b5ddb319603ce6c5a5ad7c4228eb1f02ad9483
Ali Zulqarnain <warraichleopard@gmail.com>
2025-04-23 17:06:33 +0500

    fix: added vercel config

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -9,8 +9,6 @@ import placesData from "@/constants/places.json";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
-export const maxDuration = 60;
-
 const placesContext = `
         Cities Data:
         ${JSON.stringify(citiesData)}
